a0a8e757361f5079febc47bd5d4670533c313952
Genady <46424124+genakogan@users.noreply.github.com>
2023-05-07 20:05:43 +0300

    Add files via upload

---
+++ Labs/signals/kill.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void quitproc()
+{
+    printf("\n i am child: my father ended me \n");
+    exit(0);
+}
+
+int main()
+{
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    }
+    else if (pid == 0){
+        signal(SIGQUIT, quitproc);
+        printf("\n i am child: waiting for signal \n");
+        for(;;);
+    }
+    else{
+        printf("\n i am father : i send signal quit to my child \n");
+        sleep(1);
+        kill(pid , SIGQUIT);
+        wait(NULL); // Wait for child to exit
+    }
+    return 0;
+}
