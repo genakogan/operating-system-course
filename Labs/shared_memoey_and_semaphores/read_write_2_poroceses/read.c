2798681666e730371030e729ce33c0b5652c4cd9
genakogan <rgkogan@gmail.com>
2024-03-27 16:54:56 +0200

    write and read

--- Labs/shared_memoey_and_semaphores/read_write_2_poroceses/read.c
+++ Labs/shared_memoey_and_semaphores/read_write_2_poroceses/read.c
@@ -6,13 +6,10 @@
 #include <stdlib.h>
 #include <wait.h>
 #define SEGSIZE 100
-
-
-
 int main()
 {
 	// ftok to generate unique key
-	key_t key = ftok("t.txt", 65);
+	key_t key = ftok("shmfile", 65);
 
 	// shmget returns an identifier in shmid
 	int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
@@ -20,15 +17,14 @@ int main()
 	// shmat to attach to shared memory
 	char* str = (char*)shmat(shmid, (void*)0, 0);
 
-	printf("Write Data\n");
-	strcpy(str,"Hello i write to shared memory");
-
-	printf("Data written in memory: %s\n" str);
+	printf("Data read from memory:%s \n", str);
 
 	// detach from shared memory
 	shmdt(str);
 
+	// destroy the shared memory
+	shmctl(shmid, IPC_RMID, NULL);
+
 	return 0;
 }
-
 
