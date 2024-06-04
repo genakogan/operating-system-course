f76fbf29f3191b128a8793a9f1662005aa1faf3d
genakogan <rgkogan@gmail.com>
2024-06-04 11:08:19 +0300

    c example

---
+++ Labs/basic-commands/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int args, char *argv[]){
+    printf("lab1\n");
+    printf("args %d, argv %s",args, argv[1]); 
+    for(int i =0; i<args; i++)
+        printf("arg %d: %s\n",i, argv[i]);
+    return 0;
+}
+
